924ccee1228a766fb0e08d30b28535eb12f46908
Maximo Gomez Pessoa <maximogomezpessoa@gmail.com>
2026-04-04 13:01:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // Abre la caja fuerte de las variables ocultas
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const mongoose = require('mongoose'); // <-- El traductor de Base de Datos
 
 const app = express();
 const server = http.createServer(app);
@@ -10,8 +12,30 @@ const io = new Server(server, {
 
 app.use(express.static('public'));
 
-let historialDibujos = [];
-const PASSWORD_SALA = "maxigp01";
+// --- 1. CONEXIÓN A LA BASE DE DATOS ---
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri)
+    .then(() => console.log("🟢 ¡Conectado a MongoDB! El cerebro está en línea."))
+    .catch(err => console.error("🔴 Error conectando a MongoDB:", err));
+
+// --- 2. EL "MOLDE" DE LOS PROYECTOS (SCHEMA) ---
+// Así es como se guardará cada plantilla en la nube
+const ProyectoSchema = new mongoose.Schema({
+    nombre: String,          // Nombre del archivo (ej. "Mapa Mental")
+    elementos: Array,        // Aquí guardaremos el JSON gigante de los dibujos
+    fecha: { type: Date, default: Date.now }
+});
+
+// Creamos el modelo basado en el molde
+const Proyecto = mongoose.model('Proyecto', ProyectoSchema);
+// ----------------------------------------------
+
+
+// Por ahora mantenemos esta variable en RAM para que tu pizarra actual 
+// siga funcionando mientras terminamos de armar el sistema de archivos.
+let historialDibujos = []; 
+const PASSWORD_SALA = "12345";
 
 io.use((socket, next) => {
     const password = socket.handshake.auth.password;
@@ -37,17 +61,13 @@ io.on('connection', (socket) => {
         io.emit('limpiar_todo');
     });
 
-    // Evento exclusivo para el Láser (No se guarda en el historial, solo se transmite)
     socket.on('dibujar_laser', (datos) => {
         socket.broadcast.emit('dibujar_laser', datos);
     });
 
     socket.on('mover_cursor', (datos) => {
         socket.broadcast.emit('mover_cursor', { 
-            id: socket.id, 
-            x: datos.x, 
-            y: datos.y, 
-            nombre: datos.nombre // <-- Ahora transmitimos el nombre
+            id: socket.id, x: datos.x, y: datos.y, nombre: datos.nombre 
         });
     });
 
